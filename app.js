184fe0de162f81a6795aa1e90e26061f7b965664
gabminamedez <gabminamedez@gmail.com>
2020-12-11 10:15:24 +0800

    Established connection to remote DB

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const port = 8080;
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "akosigabo",
-    database: "airbnb"
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DB
 });
 
 db.connect((err) => {
